b3ad5c5bcfc638f7b77c18b9cd3145cd2c9265d4
ichmarah <ichmarah@gmail.com>
2020-04-21 20:29:44 +0200

    Learn dictionaries

---
+++ chapter_4/dictionaries.py
@@ -0,0 +1,14 @@
+# When checking if key or value exists in a dictionary, we can use the get() method in order to prevent an error message (which breaks the code)
+
+eggs = {'name': 'Zophie', 'species': 'cat', 'age': 8}
+# eggs['color'] will give an error
+
+print(eggs)
+print(eggs.get('age', 0)) # gives 8. If there was no 'age, the fallback default value will be added to the dictionary if the key doesn't exist in dictionary --> age': 0
+
+#===============================================
+
+# Opposite of get() method: setdefault() method: to set a value in dict for a certain key only if that key is not already set
+eggs.setdefault('color', 'orange')
+print(eggs) # now 'color': 'orange' is added to the dict.
+# If we call setdefault('color', 'orange'), nothing happens becuase color is already set.
