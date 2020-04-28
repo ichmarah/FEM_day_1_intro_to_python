c7ca6d501c6cb4e92b1cc9f6ff5bdd277962e398
ichmarah <ichmarah@gmail.com>
2020-04-28 18:02:23 +0200

    Learn strptime

--- Microsoft/15_date_data_types.py
+++ Microsoft/15_date_data_types.py
@@ -12,3 +12,20 @@ print(f'Yesterday\'s date was {yesterday}')
 seven_days = timedelta(days=7)
 last_week = today - seven_days
 print(f'Last week\'s date is {last_week}')
+
+#=====================================================
+
+# Print current date in day, month, year
+print(f'Day: {today.day}')
+print(f'Month: {today.month}')
+print(f'Year: {today.year}')
+
+#=====================================================
+
+#Print users input birthday
+birthday = input('When is your birthday (dd/mm/yyyy)?')
+
+birthday_date = datetime.strptime(birthday, '%d/%m/%Y') # If user provides only two last digit in input(), there is a system error
+
+#birthday_date = datetime.strptime(birthday, '%d/%m/%y') small letter 'y' gives only the last two digits of the birth year
+print(f'Birthday: {str(birthday_date)}')
