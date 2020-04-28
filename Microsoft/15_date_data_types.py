14fb5ba9d0544bf4f15ff7431e0635417d9e306d
ichmarah <ichmarah@gmail.com>
2020-04-28 21:38:41 +0200

    Dynamic coding w/ datetime library

--- Microsoft/15_date_data_types.py
+++ Microsoft/15_date_data_types.py
@@ -10,8 +10,12 @@ yesterday = today - one_day
 print(f'Yesterday\'s date was {yesterday}')
 
 seven_days = timedelta(days=7)
+# You can use 'weeks' as well instead of the above code
+one_week = timedelta(weeks=1)
 last_week = today - seven_days
-print(f'Last week\'s date is {last_week}')
+last_week2 = today - one_week
+print(f'Last week\'s date was {last_week}')
+print(f'Last week was {last_week2} – (using \'weeks=1\' in timedelta())')
 
 #=====================================================
 
@@ -24,8 +28,10 @@ print(f'Year: {today.year}')
 
 #Print users input birthday
 birthday = input('When is your birthday (dd/mm/yyyy)?')
-
-birthday_date = datetime.strptime(birthday, '%d/%m/%Y') # If user provides only two last digit in input(), there is a system error
+# strp strips the elements form out of the string birthday and converts it into adatetime format
+birthday_date = datetime.strptime(birthday, '%d/%m/%Y') 
+birth_year = birthday_date.year
+# If user provides only two last digit in input(), there is a system error
 
 #birthday_date = datetime.strptime(birthday, '%d/%m/%y') small letter 'y' gives only the last two digits of the birth year
-print(f'Birthday: {str(birthday_date)}')
+print(f'Birthday: {birthday_date}\nBirth year: {birth_year}')
