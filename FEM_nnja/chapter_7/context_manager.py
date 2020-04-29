c250650bcda91b8a73b3aed7e0fd24b6352aeac0
ichmarah <ichmarah@gmail.com>
2020-04-29 20:08:32 +0200

    Add comment in chapter_7, Context Manager

--- FEM_nnja/chapter_7/context_manager.py
+++ FEM_nnja/chapter_7/context_manager.py
@@ -1,7 +1,9 @@
 # Context Manager  is like a wrapper around a block of code that depends on some resource. It’s a safer way of handling resources than, say, using open() and then remembering to close() later (and hoping your program doesn’t crash in between). 
 
+# json library is imported
 # json data is downloaded from https://www.learnpython.dev/code/cities.json
 # The relative path of the file should be used
+
 import json
 with open("FEM_nnja/chapter_7/cities.json") as cities_files:
   # Parse json data to readable text
