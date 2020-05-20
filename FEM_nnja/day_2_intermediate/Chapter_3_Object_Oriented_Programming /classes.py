abfa39e62aa10714a84ae1744c313e999c00d799
ichmarah <ichmarah@gmail.com>
2020-05-20 18:22:01 +0200

    Classes exercise

--- FEM_nnja/day_2_intermediate/Chapter_3_Object_Oriented_Programming /classes.py
+++ FEM_nnja/day_2_intermediate/Chapter_3_Object_Oriented_Programming /classes.py
@@ -1,9 +1,29 @@
-# ============================ self  ============================
-class Car:
-    runs = True
+# ============================ self  ===========================
+
+
+class Car():
+    runs = False
 
     def start(self):
-        if (self):
-            print("The car is started. Vroom, vroom, let's go! 🚗")
+        if self.runs:
+            print("The car is started. Vroom, vroom, let's go!")
         else:
-            print("The car is broken. ☹️")
+            print("The car is broken.")
+
+
+my_car = Car()
+print(f"Runs is set to False: {my_car.runs}")
+# Runs is set to False: False
+my_car.start()
+# The car is broken.
+
+my_car.runs = True
+print(f"Runs is later set to True: {my_car.runs}")
+my_car.start()
+# Runs is later set to True: True
+# The car is started. Vroom, vroom, let's go!
+
+my_other_car = Car()
+my_other_car.start()
+# The car is broken. --> Because this is another instance of Car() and refers to rhe original runs boolean False
+
