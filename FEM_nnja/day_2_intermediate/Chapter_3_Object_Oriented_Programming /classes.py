b90cdc588c4d0374d31348f6de468f8e68c95ee6
ichmarah <ichmarah@gmail.com>
2020-05-20 17:30:55 +0200

    Rename dir

---
+++ FEM_nnja/day_2_intermediate/Chapter_3_Object_Oriented_Programming /classes.py
@@ -0,0 +1,9 @@
+# ============================ self  ============================
+class Car:
+    runs = True
+
+    def start(self):
+        if (self):
+            print("The car is started. Vroom, vroom, let's go! 🚗")
+        else:
+            print("The car is broken. ☹️")
